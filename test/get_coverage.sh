3beb5e7d29cde49926b49107b63a61f65ccfc833
Maximilian Stadler <mstadler.nvidia.com>
2023-07-13 06:26:14 -0700

    add excludes

--- test/get_coverage.sh
+++ test/get_coverage.sh
@@ -4,7 +4,9 @@
 coverage run \
 --rcfile='coverage.pytest.rc' \
 -m pytest \
---ignore=derivs_test.py
+--ignore=derivs_test.py \
+--ignore=test_meshgraphnet_snmg.py \
+--ignore=test_graphcast_snmg.py
 
 coverage run \
 --rcfile='coverage.docstring.rc' \
